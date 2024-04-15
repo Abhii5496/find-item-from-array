debe82a5cfa39cd19b4e6e73d25687f38fd2b7a9
Abhishek <abhishekhsharma2002@gmail.com>
2024-04-15 11:52:13 +0530

    code update

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
-const findItem = (array , arg) => {
-    const val = array && array.length > array.find(item => item == arg)
-    return val
-}
+const findItem = (arr , arg) => Array.isArray(arr) && arr.length > 0 && arr.some(item => item === arg)
 
 module.exports = findItem
 
-// console.log(findItem([1,2,3,4,5], '5'))
+// console.log(findItem([1,2,3,4,5], 5))
